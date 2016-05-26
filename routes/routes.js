61c45f253ca1f594dbbe0f401424a11cb1db2f2f
Kulraj Suri <kulraj.suri@gmail.com>
2016-05-26 15:06:52 +0100

    irr added
    - added irr
    - fixed scrolling when years is high number

--- routes/routes.js
+++ routes/routes.js
@@ -22,6 +22,10 @@ var appRouter = function(app){
     app.get("/npv", function(req, res){
         res.sendFile('index.html', {root: __dirname + '/../templates'});
     });
+    
+    app.get("/irr", function(req, res){
+        res.sendFile('irr.html', {root: __dirname + '/../templates'});
+    });
 
     app.post("/npv", function(req, res){
         var discountRate = req.body.discountRate;
@@ -34,9 +38,31 @@ var appRouter = function(app){
         } else {
             var finance = new Finance();
             var npvValueFromLibrary = finance.NPV2(discountRate, initialInvestment, years, cashFlow);
-            var npvRounded = math.round(npvValueFromLibrary, 2);
-            var npvValue = {"npv": npvRounded};
+//            var npvRounded = math.round(npvValueFromLibrary, 2);
+            var npvValue = {"npv": math.round(npvValueFromLibrary, 2)};
             return res.send(npvValue);       
+        }
+    });
+    
+    app.post("/irr", function(req, res){
+        var initialInvestment = req.body.initialInvestment;
+        var years = req.body.years;
+        var cashFlow = req.body.cashFlow;
+
+        if(!initialInvestment || !years || !cashFlow) {
+            return res.send({"status": "error", "message": "missing a parameter"});
+        } else {
+            var finance = new Finance();
+            cashFlow = [200000, 300000, 200000]
+            console.log(cashFlow);
+            console.log(cashFlow[0]);
+//            var irrValueFromLibrary = finance.IRR(-initialInvestment, cashFlow[0], cashFlow[1], cashFlow[2]);
+            var irrValueFromLibrary = finance.IRR(-initialInvestment, cashFlow);
+
+            console.log(irrValueFromLibrary);
+//            var irrRounded = math.round(irrValueFromLibrary, 2);
+            var irrValue = {"irr": math.round(irrValueFromLibrary, 2)};
+            return res.send(irrValue);       
         }
     });
 };
