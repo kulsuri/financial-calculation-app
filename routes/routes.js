3ddce9b683aefd445932f1bd76a71625d955728c
Kulraj Suri <kulraj.suri@gmail.com>
2016-06-01 15:35:13 +0100

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,27 @@
-var seneca = require('seneca')();
+var seneca = require('seneca')()
 seneca.client ({
     host: 'localhost',
     port: '3001',
     pin: {role: 'finance'}
-});
+})
 var appRouter = function(app){
-    
+
     app.get("/", function(req, res){
         res.send("this is the homepage!!");
     });
-    
+
     app.get("/about", function(req, res){
         res.send("this is about");
     });
-    
+
     app.get("/contact", function(req, res){
         res.send("this is contact");
     });
-    
+
     app.get("/home", function(req, res){
         res.sendFile('test.html', {root: __dirname + '/../templates'});
     });
-    
+
     app.get("/npv", function(req, res){
         res.sendFile('index.html', {root: __dirname + '/../templates'});
     });
@@ -35,7 +35,7 @@ var appRouter = function(app){
         var initialInvestment = req.body.initialInvestment;
         var years = req.body.years;
         var cashFlow = req.body.cashFlow;
-        
+
         if(!discountRate || !initialInvestment || !years || !cashFlow) {
             return res.send({"status": "error", "message": "missing a parameter"});
         } else {
